7bbad6f84a88e663eb0d8655f95c774bd28ed2ab
JessOdette <A01643724@tec.mx>
2024-04-24 04:34:04 -0600

    barra de salud integrada

--- Assets/Scripts/playerInteraction.cs
+++ Assets/Scripts/playerInteraction.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Necesario para trabajar con UI
 
 public class PlayerController : MonoBehaviour
 {
-    public int health = 3; 
+    public int health = 10;
     public int maxHealth = 10;
+    public Image healthBarForeground; // Referencia al componente Image del indicador de salud
 
-void Start ()
-{
-    health = maxHealth;
-}
-
-
-public void TakeDamage(int amount)
-{
-    health -= amount;
-    if (health <= 0)
+    void Start ()
     {
-        Destroy(gameObject);
+        health = maxHealth;
+        UpdateHealthBar();
     }
-}
 
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            UpdateHealthBar(); // Actualiza la barra de salud cada vez que el jugador recibe daño
+        }
+    }
 
+    void UpdateHealthBar()
+    {
+        
+        float healthRatio = (float)health / maxHealth;
+        healthBarForeground.fillAmount = healthRatio;
 
-
+    }
 }
